57ccbf76d1097d0cef3c6a8b15ee4db5db570960
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-03-12 23:15:37 +0530

    Update unionSize.cpp

--- unionSize.cpp
+++ unionSize.cpp
@@ -6,17 +6,17 @@ class DSU{
     public:
       DSU(int n){
         parent.resize(n);
-        size().resize(n);
+        size.resize(n);
         numberOfComponents;
         for (int i=0;i<n;i++){
             parent[i]=i;
-            size[i]=1
+            size[i]=1;
         }
       }
 
       int find(int x){
         if (x==parent[x]) return x;
-        return pareant[x]=find(parent[x]);
+        return parent[x]=find(parent[x]);
       }
 
       void Union(int x,int y){
@@ -24,10 +24,10 @@ class DSU{
         int y_parent=parent[y];
         if (x_parent==y_parent) return;
 
-        if (size[x_parent]>size(y_parent)){
+        if (size[x_parent]>size[y_parent]){
             parent[y_parent]=x_parent;
             size[x_parent]+=parent[y_parent];
-        } else if (size[y_parent]>size(x_parent)){
+        } else if (size[y_parent]>size[x_parent]){
             parent[x_parent]=y_parent;
             size[y_parent]+=parent[x_parent];
         }  else {
