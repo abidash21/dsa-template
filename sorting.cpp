a59482c09494fcaa58e2bb1e1b7bc75757da24e9
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-09-29 14:23:11 +0000

    Time Complexity for best case

--- sorting.cpp
+++ sorting.cpp
@@ -46,3 +46,24 @@
         }
         return nums;
     }
+
+// Important point - reason of best case senerio;
+/*
+1.Selection Sort - The main idea is to find the minimum element in rest of the array 
+                   and place it to the available left most position. In every iteration 
+                   there may be different minimum element hence it is important to traverse 
+                   through the whole array every time. Hence the complexity is O(n^2).
+
+2.Bubble Sort - The main idea is to find the maximum element in rest of the array and 
+                take it to the right most position through swapping. While finding the 
+                maximum element in first attempt through swapping ,if no swap happens is done
+                int first iteration then, it is ensured that all elements are in sorted position.
+                Only inner loop will run once,hence the complexity is O(n).
+
+3.Insertion Sort - The main idea is to take an element and place it in the right place in the subarray
+                   from the starting element to the current element. While comparing current element with 
+                   previous element if it is greater then no swap will happen because all the elements before
+                   that element is already sorted order. Only outer loop will run once in case of already sorted
+                   array because everytime the inner loops starts it will exit because of the condition of while loop,
+                   so inner loop will run once for every outer loop, hence the time complexity is O(n).
+*/
