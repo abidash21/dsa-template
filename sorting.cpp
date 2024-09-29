2ad34a2325d0d094d716e865f9b7d4c32cdf7f31
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-09-29 13:56:21 +0000

    Bubble Sort

--- sorting.cpp
+++ sorting.cpp
@@ -12,3 +12,21 @@
         }
         return nums;
     }
+
+// Bubble Sort - Push the largest element to the right most position by adjacent swapping
+// Time Complexity -> Best case - O(n)
+//                    Worst case - O(n^2)
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        for (int i=n-1;i>=0;i--){
+            bool flag=0;
+            for (int j=0;j<i;j++){
+                if (nums[j]>nums[j+1]){
+                     swap(nums[j],nums[j+1]);
+                     flag=1;
+                }
+            }
+            if (flag==0) break;
+        }
+        return nums;
+    }
