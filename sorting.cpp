e256697989a572913d2ebd2ce3b06a0acd02316b
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-09-29 14:12:59 +0000

    Insertion Sort

--- sorting.cpp
+++ sorting.cpp
@@ -30,3 +30,19 @@
         }
         return nums;
     }
+
+// Insertion Sort - Take and element and place it in correct position by swapping adjacent elements in backward direction 
+// within the subarray(from starting postion till the element position)
+// Time Complexity -> Best case - O(n)
+//                    Worst case - O(n^2)
+    vector<int> sortArray(vector<int>& nums) {
+        int n=nums.size();
+        for (int i=1;i<n;i++){
+            int j=i;
+            while(j>=1 && nums[j]<nums[j-1]){
+                swap(nums[j],nums[j-1]);
+                j--;
+            }
+        }
+        return nums;
+    }
