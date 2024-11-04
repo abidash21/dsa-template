7f4358fea59a6f8af04377447315f2c9fa587046
Abinash Dash <72178983+abidash21@users.noreply.github.com>
2024-11-04 01:58:59 +0000

    Topological sort BFS

---
+++ Graph/TopoSort_BFS.cpp
@@ -0,0 +1,30 @@
+    void dfs(int node, vector<int> adj[],stack<int>&st,vector<int>&vis){
+	    vis[node]=1;
+	    for (auto it:adj[node]){
+	        if (!vis[it])
+	        dfs(it,adj,st,vis);
+	    }
+	    st.push(node);
+	}
+	
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    vector<int>vis(V,0);
+	    stack<int>st;
+	    vector<int>ans;
+	    for (int i=0;i<V;i++){
+	        if (!vis[i])
+	        dfs(i,adj,st,vis);
+	    }
+	    while(!st.empty()){
+	        int node=st.top();
+	        st.pop();
+	        ans.push_back(node);
+	    }
+	    return ans;
+	}
+
+    /*
+       TC: O(V+E)+O(V)
+       SC: O(3V)
+    */
